bbd6832fceca6e22405ca8367e748b8ac675e1fa
ly <1459097290@qq.com>
2022-03-30 19:30:23 +0800

    chore: react 18

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom'
+import ReactDOMClient from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import configureStore from 'store/configureStore'
@@ -8,11 +8,17 @@ import App from './App'
 const store = configureStore()
 store.runSaga(rootSaga)
 
-ReactDOM.render(
+const container = document.getElementById('root')
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container)
+
+// Initial render: Render an element to the root.
+root.render(
     <Provider store={store}>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
-    </Provider>, document.getElementById('root')
+    </Provider>
 )
 
